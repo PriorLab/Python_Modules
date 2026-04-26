d18606141e4f2cbf25bcea6747f21ca9bea5435d
Alexandre Miguel Prior Dias Das Neves <alemigue@c1r3s5.42porto.com>
2026-04-26 17:42:50 +0100

    half way ex6 M1

--- 01_Module_01/v2/ex6/ft_garden_analytics.py
+++ 01_Module_01/v2/ex6/ft_garden_analytics.py
@@ -10,26 +10,40 @@ class Stats:
     def add_age_call(self) -> None:
         self._age_calls += 1
 
-    def add
-class Plant:
+    def add_show_call(self) -> None:
+        self._show_calls += 1
+
+    def display_stats(self) -> None:
+        print(
+            f"Stats: {self._grow_calls} grow, "
+            f"{self._age_calls} age, "
+            f"{self._show_calls} show"
+        )
+
+
+class Plant(Stats):
     def __init__(
         self,
         name: str,
         height: float,
         age: int,
     ):
+        super().__init__()
         self.name = name
         self.height = height
         self.age = age
 
     def growth(self, growth: float) -> None:
         self.height += growth
+        self.add_grow_call()
 
-    def older(self, days:int) -> None:
+    def older(self, days: int) -> None:
         self.age += days
+        self.add_age_call()
 
     def show(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
+        self.add_show_call()
 
     @staticmethod
     def year_old(age: int) -> None:
@@ -59,19 +73,23 @@ class Flower(Plant):
         self.bloom = True
         self.growth(8.0)
 
-    
-
-
 
 def main() -> None:
     print("=== Garden statistics ===")
     print("=== Check year-old")
-
     Plant.year_old(30)
     Plant.year_old(400)
     print("=== Flower")
     rose = Flower("Rose", 15.0, 10, "red")
     rose.show()
+    rose.bloom_state()
+    print(f"[statistics for {rose.name}]")
+    rose.display_stats()
+    rose.ask_to_bloom_grow()
+    rose.show()
+    rose.bloom_state()
+    print(f"[statistics for {rose.name}]")
+    rose.display_stats()
 
 
 if __name__ == "__main__":
